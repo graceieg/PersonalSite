3f0861cef5317c070d0fa28706fc624235ad68f9
Grace Giebel <gracearlayna@gmail.com>
2025-04-29 22:17:25 -0400

    Add Netlify adapter and configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-	base: '/PersonalSite/',
-	site: 'https://example.com',
+	site: 'https://gracearlayna.netlify.app/',
 	integrations: [mdx(), sitemap()],
+	adapter: netlify()
 });
