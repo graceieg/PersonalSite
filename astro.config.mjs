9b091710b42e6cddc1c09bc916d9d186b699756d
Grace Giebel <gracearlayna@gmail.com>
2025-04-29 23:00:55 -0400

    Add image test page and fix image path issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,10 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import netlify from '@astrojs/netlify';
 
-// https://astro.build/config
 export default defineConfig({
-	site: 'https://gracearlayna.netlify.app/',
-	integrations: [mdx(), sitemap()],
-	adapter: netlify()
+  site: 'https://gracearlayna.netlify.app/',
+  integrations: [mdx(), sitemap()],
+  adapter: netlify({
+    imageCDN: true // Enable Netlify Image CDN
+  })
 });
