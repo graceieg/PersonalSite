9a8a8a8bf68e8b600065e82ee6a08daec717c83e
User Name <user@example.com>
2025-09-07 16:20:34 -0400

    feat: add decrypted text effect to education section and update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,11 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import netlify from '@astrojs/netlify';
 
+import react from '@astrojs/react';
+
 export default defineConfig({
   site: 'https://gracearlayna.netlify.app/',
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), react()],
   adapter: netlify({
     imageCDN: true // Enable Netlify Image CDN
   })
